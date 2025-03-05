b04401ce1e9df195206ba95e1fe850725b6aea5e
Seongwon Jo <tjddnjsjo@kakao.com>
2025-03-06 03:27:40 +0900

    feat: update README.md with staleTime and gcTime explanations; set staleTime in Posts component

--- blog-em-ipsum/src/Posts.jsx
+++ blog-em-ipsum/src/Posts.jsx
@@ -13,6 +13,7 @@ export function Posts() {
   const { data, isError, error, isLoading } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
+    staleTime: 2000,
   });
 
   if (isLoading) {
