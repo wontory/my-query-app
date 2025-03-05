5faabd062f67877ce7543c3addff39a80b444fcf
Seongwon Jo <tjddnjsjo@kakao.com>
2025-03-06 02:58:06 +0900

    feat: enhance error handling and loading states in Posts component; update README.md

--- blog-em-ipsum/src/Posts.jsx
+++ blog-em-ipsum/src/Posts.jsx
@@ -10,13 +10,22 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data } = useQuery({
+  const { data, isError, error, isLoading } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
   });
 
-  if (!data) {
-    return <div />;
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
   }
 
   return (
--- blog-em-ipsum/src/api.js
+++ blog-em-ipsum/src/api.js
@@ -2,6 +2,7 @@ export async function fetchPosts(pageNum = 1) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
+  // throw new Error("you cannot have this data");
   return response.json();
 }
 
