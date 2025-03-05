0cc4310f09595e555978fea36bd6ed43edde54a5
Seongwon Jo <tjddnjsjo@kakao.com>
2025-03-06 02:44:36 +0900

    feat: integrate TanStack Query for fetching posts and update constant naming

--- blog-em-ipsum/src/Posts.jsx
+++ blog-em-ipsum/src/Posts.jsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
-const maxPostPage = 10;
+
+const MAX_POST_PAGE = 10;
 
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const { data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (!data) {
+    return <div />;
+  }
 
   return (
     <>
