17f82ffae819c9cb3f96b014aab163dea9654fdc
Seongwon Jo <tjddnjsjo@kakao.com>
2025-03-07 00:43:08 +0900

    feat: implement useQuery for fetching comments in PostDetail component and update README.md with query key explanations

--- blog-em-ipsum/src/PostDetail.jsx
+++ blog-em-ipsum/src/PostDetail.jsx
@@ -1,9 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+
 import { fetchComments } from "./api";
 import "./PostDetail.css";
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["comments", post.id],
+    queryFn: () => fetchComments(post.id),
+  });
+
+  if (isLoading) {
+    return <h3>Loading!</h3>;
+  }
+
+  if (isError) {
+    return (
+      <>
+        <h3>Error</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
