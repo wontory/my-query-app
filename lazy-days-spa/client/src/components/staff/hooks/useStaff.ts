42b8deb98712ca5b7740dabad3d2ea3cd7a802c6
Seongwon Jo <tjddnjsjo@kakao.com>
2025-03-09 03:55:05 +0900

    feat: implement useQuery for fetching staff data in useStaff hook

--- lazy-days-spa/client/src/components/staff/hooks/useStaff.ts
+++ lazy-days-spa/client/src/components/staff/hooks/useStaff.ts
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
 import type { Staff } from "@shared/types";
@@ -7,18 +8,20 @@ import { filterByTreatment } from "../utils";
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 
-// query function for useQuery
-// async function getStaff(): Promise<Staff[]> {
-//   const { data } = await axiosInstance.get('/staff');
-//   return data;
-// }
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get("/staff");
+  return data;
+}
 
 export function useStaff() {
-  // for filtering staff by treatment
+  const fallback: Staff[] = [];
+
   const [filter, setFilter] = useState("all");
 
-  // TODO: get data from server via useQuery
-  const staff: Staff[] = [];
+  const { data: staff = fallback } = useQuery({
+    queryKey: [queryKeys.staff],
+    queryFn: getStaff,
+  });
 
   return { staff, filter, setFilter };
 }
