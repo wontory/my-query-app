bac0a502208e8adb613299ffb0809923a224199b
Seongwon Jo <tjddnjsjo@kakao.com>
2025-03-10 00:33:06 +0900

    test: update AllStaff.test to verify rendering of staff titles with async query handling

--- lazy-days-spa/client/src/components/staff/tests/AllStaff.test.tsx
+++ lazy-days-spa/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,7 +1,13 @@
 import { AllStaff } from "../AllStaff";
 
-// import { render, screen } from "@/test-utils";
+import { render, screen } from "@/test-utils";
 
-test("renders response from query", () => {
-  // write test here
+test("renders response from query", async () => {
+  render(<AllStaff />);
+
+  const staffTitles = await screen.findAllByRole("heading", {
+    name: /sandra|divya|mateo|michael/i,
+  });
+
+  expect(staffTitles).toHaveLength(4);
 });
