e48b2d8493e2fcd1e9dc1fc67e488143d92252dc
Seongwon Jo <tjddnjsjo@kakao.com>
2025-03-10 01:10:33 +0900

    test: enhance useStaff.test to verify staff filtering functionality

--- lazy-days-spa/client/src/components/staff/tests/useStaff.test.tsx
+++ lazy-days-spa/client/src/components/staff/tests/useStaff.test.tsx
@@ -1,9 +1,17 @@
-// import { act, renderHook, waitFor } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 
 import { useStaff } from "../hooks/useStaff";
 
-// import { createQueryClientWrapper } from "@/test-utils";
+import { createQueryClientWrapper } from "@/test-utils";
 
 test("filter staff", async () => {
-  // the magic happens here
+  const { result } = renderHook(() => useStaff(), {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  await waitFor(() => expect(result.current.staff).toHaveLength(4));
+
+  act(() => result.current.setFilter("facial"));
+
+  await waitFor(() => expect(result.current.staff).toHaveLength(3));
 });
