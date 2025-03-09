006fd5ae378db8c86cb8cdb74fe3d5737aa224e3
Seongwon Jo <tjddnjsjo@kakao.com>
2025-03-09 23:11:58 +0900

    feat: implement useReserveAppointment hook with mutation for reserving appointments

--- lazy-days-spa/client/src/components/appointments/hooks/useReserveAppointment.ts
+++ lazy-days-spa/client/src/components/appointments/hooks/useReserveAppointment.ts
@@ -1,3 +1,5 @@
+import { useMutation } from "@tanstack/react-query";
+
 import type { Appointment } from "@shared/types";
 
 import { useLoginData } from "@/auth/AuthContext";
@@ -5,26 +7,30 @@ import { axiosInstance } from "@/axiosInstance";
 import { useCustomToast } from "@/components/app/hooks/useCustomToast";
 import { queryKeys } from "@/react-query/constants";
 
-// for when we need functions for useMutation
-// async function setAppointmentUser(
-//   appointment: Appointment,
-//   userId: number | undefined,
-// ): Promise<void> {
-//   if (!userId) return;
-//   const patchOp = appointment.userId ? 'replace' : 'add';
-//   const patchData = [{ op: patchOp, path: '/userId', value: userId }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+async function setAppointmentUser(
+  appointment: Appointment,
+  userId: number | undefined
+): Promise<void> {
+  if (!userId) return;
+  const patchOp = appointment.userId ? "replace" : "add";
+  const patchData = [{ op: patchOp, path: "/userId", value: userId }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+}
 
 export function useReserveAppointment() {
   const { userId } = useLoginData();
 
   const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  const { mutate } = useMutation({
+    mutationFn: (appointment: Appointment) =>
+      setAppointmentUser(appointment, userId),
+    onSuccess: () => {
+      toast({ title: "You have reserved an appointment!", status: "success" });
+    },
+  });
+
+  return mutate;
 }
